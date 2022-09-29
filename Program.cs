a87e27f9c3f0f7f296c091b2469898d69f965adf
banrj <eh.ehr@bk.ru>
2022-09-29 16:21:09 +0200

    2.2 lab

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Богомолв Даниил 13 вариант базовый
+try
+{
+    Console.WriteLine("Введите число A: ");
+    int FirstNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите число B: ");
+    int SecondNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите число C: ");
+    int ThirdNumber = int.Parse(Console.ReadLine());
+
+    if ((FirstNumber < 45 && SecondNumber > 45 && ThirdNumber > 45) ||
+        (FirstNumber > 45 && SecondNumber > 45 && ThirdNumber < 45) ||
+        (FirstNumber > 45 && SecondNumber < 45 && ThirdNumber > 45))
+    {
+        Console.WriteLine(true);
+    }
+    else Console.WriteLine(false);
+}
+
+catch
+{
+
+}
