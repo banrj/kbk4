2bcc55cd34f1b65b03626517153098981c36d03d
Bogomolov Daniil <105605702+banrj@users.noreply.github.com>
2022-10-13 09:42:36 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿// Богомолв Даниил 13 вариант базовый
+// Богомолв Даниил 13 вариант базовый
 try
 {
     Console.WriteLine("Введите число A: ");
@@ -8,13 +8,12 @@ try
     Console.WriteLine("Введите число C: ");
     int ThirdNumber = int.Parse(Console.ReadLine());
 
-    if ((FirstNumber < 45 && SecondNumber > 45 && ThirdNumber > 45) ||
-        (FirstNumber > 45 && SecondNumber > 45 && ThirdNumber < 45) ||
-        (FirstNumber > 45 && SecondNumber < 45 && ThirdNumber > 45))
-    {
-        Console.WriteLine(true);
-    }
-    else Console.WriteLine(false);
+    if (FirstNumber > SecondNumber && FirstNumber > ThirdNumber)
+        Console.WriteLine("Самое большое число " + FirstNumber);
+    else if ((FirstNumber < SecondNumber && SecondNumber > ThirdNumber))
+        Console.WriteLine("Самое большое число " + SecondNumber);
+    else if (ThirdNumber > SecondNumber && FirstNumber < ThirdNumber)
+        Console.WriteLine("Самое большое число " + ThirdNumber);
 }
 
 catch
